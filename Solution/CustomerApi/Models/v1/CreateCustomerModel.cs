54094f4c8fe1b735e89602b7b8bfd232696552b5
SapirZalach <sapirzala1@gmail.com>
2020-08-18 14:17:10 +0300

    first commit

---
+++ Solution/CustomerApi/Models/v1/CreateCustomerModel.cs
@@ -0,0 +1,21 @@
+﻿using CustomerApi.Service.v1.Query;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Cache;
+
+namespace CustomerApi.Models.v1
+{
+    public class CreateCustomerModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public int ?Age { get; set; }
+
+    }
+}
